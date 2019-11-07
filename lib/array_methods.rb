5798691a242629074653f11d2458c89e4ac95055
Taryn Johnson <taryn.johnson919@gmail.com>
2019-11-07 00:55:10 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,13 +1,18 @@
 def using_include(array, element)
-
+    using_include = ["wow", "I", "am", "really", "learning", "arrays!"]
+   
+    using_include.include? ("wow")
+    using_include.include? ("Tardis")
 end
 
 def using_sort(array)
-
+  using_sort=["wow", "I", "am", "really", "learning", "arrays!"]
+  using_sort.sort
 end
 
 def using_reverse(array)
-
+  using_reverse=["wow", "I", "am", "really", "learning", "arrays!"]
+  using_reverse.reverse 
 end
 
 def using_first(array)
